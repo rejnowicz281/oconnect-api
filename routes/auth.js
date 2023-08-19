c673e218cc9a3e76ba36e325398277ba89aafbff
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-19 14:27:37 +0200

    Implement temporary /protected route for testing

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const passport = require("passport");
 
 const router = express.Router();
 
@@ -8,5 +9,8 @@ router.post("/register", register);
 router.post("/login", login);
 router.post("/demo", demoLogin);
 router.post("/logout", logout);
+router.get("/protected", passport.authenticate("jwt", { session: false }), (req, res) => {
+    res.status(200).json({ message: "Protected Route", user: req.user.email });
+});
 
 module.exports = router;
