3949e7dbd0f590e5989e24052f7e5cec22a8812e
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-09-01 13:32:31 +0200

    Before saving user, capitalize his first_name and last_name

--- models/user.js
+++ models/user.js
@@ -32,4 +32,11 @@ const userSchema = new Schema({
     },
 });
 
+// Capitalize first letter of first and last name
+userSchema.pre("save", function (next) {
+    this.first_name = this.first_name.charAt(0).toUpperCase() + this.first_name.slice(1);
+    this.last_name = this.last_name.charAt(0).toUpperCase() + this.last_name.slice(1);
+    next();
+});
+
 module.exports = mongoose.model("User", userSchema);
